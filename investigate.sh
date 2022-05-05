9519d7654d3d1ac96d9b79967a535aa2dcd5cd33
JavaCommons Technologies <javacommons@gmail.com>
2022-05-05 15:39:31 +0900

    .

--- investigate.sh
+++ investigate.sh
@@ -31,6 +31,7 @@ scoop install git
 scoop bucket add main
 scoop bucket add extras
 json=`cat ./investigate.json`
+processApp $cwd qt-creator "/bin"
 processApp $cwd zulu17-jdk "/bin"
 processApp $cwd llvm "/bin"
 processApp $cwd nu "."
